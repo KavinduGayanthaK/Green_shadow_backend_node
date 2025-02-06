41d32ca72276737db9e0efc8d0582c74d0a8fde1
Kavindu <gayanthakavindu3@gmail.com>
2025-02-07 01:18:06 +0530

    created log schema

---
+++ schema/LogSchema.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export type LogType = "NORMAL" | "DANGER";
+
+export interface ILog extends Document {
+  logCode: string;
+  logDate: string | null;
+  logDetails: string;
+  logType: LogType;
+  logImage: string;
+  logFields: string[];
+  logCrops: string[];
+  logStaff: string[];
+}
+
+const log = new Schema<ILog>({
+  logCode: { type: String, required: true, unique: true },
+  logDate: { type: String, required: true },
+  logDetails: { type: String, required: true },
+  logType: { type: String, required: true },
+  logImage: { type: String, required: true },
+  logFields: [{ type: mongoose.Schema.Types.ObjectId, ref: "Field" }],
+  logCrops: [{ type: mongoose.Schema.Types.ObjectId, ref: "Crop" }],
+  logStaff: [{ type: mongoose.Schema.Types.ObjectId, ref: "Staff" }],
+});
+
+const Log = mongoose.model<any>("Log", log);
+export default Log;

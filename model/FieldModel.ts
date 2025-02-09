0179307bff117139d7fc1896efd31bf4d1214176
Kavindu <gayanthakavindu3@gmail.com>
2025-02-09 17:15:14 +0530

    creatd field model

---
+++ model/FieldModel.ts
@@ -0,0 +1,24 @@
+export class StaffModel {
+    fieldCode:string;
+    fieldName:string;
+    fieldLocation:string;
+    extentSizeOfTheField:string;
+    fieldCrops:string[];
+    fieldStaff:string[];
+    fieldLogs:string[];
+    fieldImage:string;
+
+    constructor(fieldCode:string,fieldName:string,fieldLocation:string,extentSizeOfTheField:string,
+        fieldCrops:string[],fieldStaff:string[],fieldLogs:string[],fieldImage:string) {
+
+            this.fieldCode = fieldCode;
+            this.fieldName = fieldName;
+            this.fieldLocation = fieldLocation;
+            this.extentSizeOfTheField = extentSizeOfTheField;
+            this.fieldCrops = fieldCrops;
+            this.fieldStaff = fieldStaff;
+            this.fieldLogs = fieldLogs;
+            this.fieldImage = fieldImage;
+           
+        }
+}

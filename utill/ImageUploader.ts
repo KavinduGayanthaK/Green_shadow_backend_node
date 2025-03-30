b6613f7daa387c115354f7b2101ac3501e45a431
Kavindu Gayantha <117292977+KavinduGayanthaK@users.noreply.github.com>
2025-03-30 16:41:50 +0530

    change the image uploader

--- utill/ImageUploader.ts
+++ utill/ImageUploader.ts
@@ -2,15 +2,18 @@ import multer from "multer";
 import path from "path";
 
 export class ImageUploader {
-    uploader(imageType:string){
+    uploader(imageType: string) {
         const storage = multer.diskStorage({
             destination: (req, file, cb) => {
+                
                 cb(null, `uploads/${imageType}/`);
             },
             filename: (req, file, cb) => {
-                cb(null, Date.now() + path.extname(file.originalname));
+                
+                const filename = `${imageType}_${Date.now()}${path.extname(file.originalname)}`;
+                cb(null, filename);
             }
         });
-        return multer({ storage: storage});
+        return multer({ storage });
     }
 }
